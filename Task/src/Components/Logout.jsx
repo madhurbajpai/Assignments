bd4698b2368168eace4bfca93297d1649f3b5b78
madhurbajpai <mdhrbajpai@gmail.com>
2025-04-01 20:18:26 +0530

    css modified

--- Task/src/Components/Logout.jsx
+++ Task/src/Components/Logout.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { logout } from '../redux/authSlice';
-
-
+import React from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../redux/authSlice";
 
 const Logout = () => {
   const dispatch = useDispatch();
@@ -12,7 +10,21 @@ const Logout = () => {
   };
 
   return (
-    <button onClick={handleLogout}>Logout</button>
+    <button
+      onClick={handleLogout}
+      style={{
+        height: "10%",
+        marginTop: "20px",
+        marginLeft: "30px",
+        backgroundColor: "red",
+        color: "white",
+        cursor: "pointer",
+      }}
+      onMouseOver={(e) => (e.target.style.backgroundColor = "darkred")}
+      onMouseOut={(e) => (e.target.style.backgroundColor = "red")}
+    >
+      Logout
+    </button>
   );
 };
 
--- Task/src/Components/TaskList.jsx
+++ Task/src/Components/TaskList.jsx
@@ -7,15 +7,51 @@ const TaskList = () => {
   const dispatch = useDispatch();
 
   return (
-    <ul style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'end'}}>
-      {tasks.map((task) => (
-        <li key={task.id}>
-          {task.text} - {task.priority} 
-          {task.weather && ` (Weather: ${task.weather})`}
-          <button onClick={() => dispatch(deleteTask(task.id))}>Delete</button>
-        </li>
-      ))}
-    </ul>
+    <div className='show'>
+      <ul style={{ 
+        display: 'flex', 
+        flexDirection: 'column', 
+        alignItems: 'center', 
+        justifyContent: 'end', 
+        width: '100%', 
+        padding: '0', 
+        listStyleType: 'none' 
+      }}>
+        {tasks.map((task) => (
+          <li 
+            key={task.id} 
+            style={{ 
+              wordWrap: 'break-word', 
+              maxWidth: '90%', 
+              margin: '10px 0', 
+              padding: '10px', 
+              border: '1px solid #ccc', 
+              borderRadius: '5px', 
+              backgroundColor: '#f9f9f9', 
+              textAlign: 'left', 
+              color: 'black'
+            }}
+          >
+            {task.text} - {task.priority} 
+            {task.weather && ` (Weather: ${task.weather})`}
+            <button 
+              onClick={() => dispatch(deleteTask(task.id))} 
+              style={{ 
+                marginLeft: '10px', 
+                padding: '5px 10px', 
+                backgroundColor: '#ff4d4d', 
+                color: 'white', 
+                border: 'none', 
+                borderRadius: '3px', 
+                cursor: 'pointer' 
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
